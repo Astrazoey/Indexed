b76cf0c6e60f9683365d44aa8d33d494b491b1a1
Zoey <jragon.alt1@gmail.com>
2025-10-23 11:50:49 +0800

    fixed debuff resistance crash

--- src/main/java/com/astrazoey/indexed/mixins/DebuffResistanceMixin.java
+++ src/main/java/com/astrazoey/indexed/mixins/DebuffResistanceMixin.java
@@ -26,26 +26,29 @@ public abstract class DebuffResistanceMixin {
     private StatusEffectInstance modifyEffectDuration(StatusEffectInstance effect) {
         LivingEntity entity = (LivingEntity) (Object) this;
         ItemStack chestplate = getEquippedStack(EquipmentSlot.CHEST);
-        int level = Indexed.getEnchantmentValue(Indexed.DEBUFF_REDUCTION, (ServerWorld) entity.getEntityWorld(), chestplate);
-        level = level / 3; //fixes level coming back three times higher for some reason
+        if(entity.getEntityWorld() instanceof ServerWorld) {
+            int level = Indexed.getEnchantmentValue(Indexed.DEBUFF_REDUCTION, (ServerWorld) entity.getEntityWorld(), chestplate);
+            level = level / 3; //fixes level coming back three times higher for some reason
 
-        // System.out.println("LEVEL IS " + level);
-        if(level > 0) {
-            if(!effect.getEffectType().value().isBeneficial()) {
+            // System.out.println("LEVEL IS " + level);
+            if(level > 0) {
+                if(!effect.getEffectType().value().isBeneficial()) {
 
-                //System.out.println("Current duration = " + effect.getDuration() / 20);
+                    //System.out.println("Current duration = " + effect.getDuration() / 20);
 
-                int remainingDuration = effect.getDuration() - (level * 10 * 20);
-                if (remainingDuration < 0) {
-                    remainingDuration = 0;
+                    int remainingDuration = effect.getDuration() - (level * 10 * 20);
+                    if (remainingDuration < 0) {
+                        remainingDuration = 0;
+                    }
+
+                    effect = new StatusEffectInstance(effect.getEffectType(), remainingDuration, effect.getAmplifier());
+
+                    //System.out.println("Effect duration is " + effect.getDuration()/20 + " and amplifier is + " + effect.getAmplifier());
+
                 }
-
-                effect = new StatusEffectInstance(effect.getEffectType(), remainingDuration, effect.getAmplifier());
-
-                //System.out.println("Effect duration is " + effect.getDuration()/20 + " and amplifier is + " + effect.getAmplifier());
-
             }
         }
+
 
         return effect;
     }
